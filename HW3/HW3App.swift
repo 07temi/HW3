b05802db7e6e8b10dd6655d055775f0c8ca07a72
Art <artem_chernenko@ro.ru>
2022-02-26 13:39:00 +0300

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by Артем Черненко on 26.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
